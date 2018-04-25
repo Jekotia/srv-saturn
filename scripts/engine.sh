134cf14f8e2d2fa3f8ad4697253c3e91452ba041
Josh Ameli <contact@jekotia.net>
2018-04-25 15:44:32 -0400

    Updated to automatically detect if it's being run on Debian x86_64 or Raspbian Raspberry Pi 3

--- scripts/engine.sh
+++ scripts/engine.sh
@@ -1,34 +1,80 @@
 #! /bin/bash
+if [[ ! `id -u` == "0" ]] ; then
+	echo "This script requires root/sudo!"
+	exit 99
+fi
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source $DIR/conf
 
-apt-get update
+arch=""
+dist=""
+sys=""
 
-apt-get install -y \
-apt-transport-https \
-ca-certificates \
-curl \
-gnupg2 \
-software-properties-common
+function detect_arch () {
+	arch=`lscpu | grep "Architecture" | cut -d':' -f2 | tr -d '[:space:]'`
+}
 
-curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
-if ! apt-key fingerprint 0EBFCD88 ; then echo "Failed to match gpg key fingerprint!" ; exit 1 ; fi
+function detect_dist () {
+	dist=`cat /etc/os-release | grep "ID" | grep -v "VERSION_ID" | cut -d'=' -f2 | tr -d '[:space:]'`
+}
 
-add-apt-repository \
-"deb [arch=amd64] https://download.docker.com/linux/debian \
-$(lsb_release -cs) \
-stable"
+function detect_sys () {
+	if [[ "$dist" == "raspbian" && "$arch" == "armv7l" ]] ; then
+		sys="rpi3"
+	elif [[ "$dist" == "debian" && "$arch" == "x86_64" ]] ; then
+		sys="debian64"
+	else
+		echo "Failed to detect system"
+		exit 1
+	fi
+}
 
-apt-get update
-apt-get install -y docker-ce
+function setup_debian64 () {
+	apt-get update
 
-usermod -aG docker $user
+	apt-get install -y \
+	apt-transport-https \
+	ca-certificates \
+	curl \
+	gnupg2 \
+	software-properties-common
 
-docker run hello-world
+	curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
+	if ! apt-key fingerprint 0EBFCD88 ; then echo "Failed to match gpg key fingerprint!" ; exit 1 ; fi
 
-cat >> /home/$user/.profile <<EOF
+	add-apt-repository \
+	"deb [arch=amd64] https://download.docker.com/linux/debian \
+	$(lsb_release -cs) \
+	stable"
+
+	apt-get update
+	apt-get install -y docker-ce
+
+	setup_common
+}
+
+function setup_rpi3 () {
+	curl -sSL https://get.docker.com | sh
+
+	setup_common
+}
+
+function setup_common () {
+	usermod -aG docker $user
+
+	docker run hello-world
+
+	cat >> /home/$user/.profile <<EOF
 
 if [ -d "/saturn/docker/bin" ] ; then
     PATH="/saturn/docker/bin:$PATH"
 fi
 EOF
+}
+
+detect_arch
+detect_dist
+detect_sys
+
+setup_${sys}
