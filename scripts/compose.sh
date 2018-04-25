a21ca0122119f5b107ea9bdd1701a0cc24920e54
Josh Ameli <contact@jekotia.net>
2018-04-25 15:50:00 -0400

    Updated to force root. Ensures global installation instead of user

--- scripts/compose.sh
+++ scripts/compose.sh
@@ -1,4 +1,9 @@
 #! /bin/bash
+if [[ ! `id -u` == "0" ]] ; then
+        echo "This script requires root/sudo!"
+        exit 99
+fi
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source $DIR/conf
 
@@ -6,10 +11,3 @@ apt update
 apt install -y python python-pip
 
 pip install docker-compose
-
-cat >> /home/$user/.profile <<EOF
-
-if [ -d "$home/.local/bin" ] ; then
-    PATH="${home}/.local/bin:$PATH"
-fi
-EOF
