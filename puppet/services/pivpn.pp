d1eda3e791bf61ad4d5e89b2113062635730fe3c
Josh 'Jekotia' Ameli <contact@jekotia.net>
2018-05-21 19:10:17 -0400

    Updated for non-docker vpn

--- puppet/services/pivpn.pp
+++ puppet/services/pivpn.pp
@@ -4,6 +4,8 @@ user { "pivpn":
 	gid		=> "$_PIVPN_GID",
 	membership	=> "inclusive",
 	system		=> "true",
+#	managehome	=> "true",
+	home		=> "/home/pivpn",
 }
 group { "pivpn":
 	ensure		=> "present",
@@ -11,10 +13,50 @@ group { "pivpn":
 	system		=> "true",
 }
 
+file { "pivpn home folder":
+	path		=> "$_PIVPN_DATA/home/pivpn",
+	ensure		=> "directory",
+	group		=> "$_PIVPN_GID",
+	owner		=> "$_PIVPN_UID",
+	recurse		=> "true",
+}
 file { "pivpn data folder":
 	path		=> "$_PIVPN_DATA",
 	ensure		=> "directory",
 	group		=> "$_PIVPN_GID",
 	owner		=> "$_PIVPN_UID",
 	recurse		=> "true",
+}
+file { "pivpn etc/openvpn folder":
+	path		=> "$_PIVPN_DATA/etc/openvpn",
+	ensure		=> "directory",
+	group		=> "root",
+	owner		=> "root",
+	recurse		=> "true",
+}
+file { "pivpn etc/pivpn folder":
+	path		=> "$_PIVPN_DATA/etc/pivpn",
+	ensure		=> "directory",
+	group		=> "root",
+	owner		=> "root",
+	recurse		=> "true",
+}
+file { "pivpn log":
+	path		=> "$_PIVPN_DATA/etc/pivpn/install.log",
+	ensure		=> "file",
+	group		=> "$_GID",
+	owner		=> "$_UID",
+}
+
+file { "/home/pivpn":
+	ensure		=> "link",
+	target		=> "$_PIVPN_DATA/home/pivpn",
+}
+file { "/etc/pivpn":
+	ensure		=> "link",
+	target		=> "$_PIVPN_DATA/etc/pivpn",
+}
+file { "/etc/openvpn":
+	ensure		=> "link",
+	target		=> "$_PIVPN_DATA/etc/openvpn",
 }
