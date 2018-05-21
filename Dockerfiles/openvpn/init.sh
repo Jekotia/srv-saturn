5f0857a011f472365b3f1dc4d1bf891122edae38
Josh 'Jekotia' Ameli <contact@jekotia.net>
2018-05-20 20:35:20 -0400

    .

--- Dockerfiles/openvpn/init.sh
+++ Dockerfiles/openvpn/init.sh
@@ -1,8 +1,24 @@
 #! /bin/bash
 OVPN_DATA=/saturn/data/openvpn
-ovpnImage=jekotia/test:openvpn
+ovpnImage="jekotia/test:openvpn"
+cmd=docker
 
-docker run --rm -v $OVPN_DATA:/etc/openvpn $ovpnImage ovpn_genconfig -u udp://saturn.jekotia.net && \
-docker run --rm -it -v $OVPN_DATA:/etc/openvpn $ovpnImage ovpn_initpki && \
-docker run --rm -it -v $OVPN_DATA:/etc/openvpn $ovpnImage easyrsa build-client-full CLIENTNAME nopass && \
-docker run --rm -v $OVPN_DATA:/etc/openvpn $ovpnImage ovpn_getclient CLIENTNAME > CLIENTNAME.ovpn
+case $1 in
+	init)
+		$cmd run --rm -v $OVPN_DATA:/etc/openvpn $ovpnImage ovpn_genconfig -u udp://saturn.jekotia.net && \
+		$cmd run --rm -it -v $OVPN_DATA:/etc/openvpn $ovpnImage ovpn_initpki
+	;;
+	run)
+		$cmd run --name openvpn -v $OVPN_DATA:/etc/openvpn -d -p 1194:1194/udp --cap-add=NET_ADMIN $ovpnImage
+	;;
+	config)
+		shift
+		clientName="$1"
+		$cmd run --rm -it -v $OVPN_DATA:/etc/openvpn $ovpnImage easyrsa build-client-full $clientName nopass && \
+		$cmd run --rm -v $OVPN_DATA:/etc/openvpn $ovpnImage ovpn_getclient $clientName > $clientName.ovpn
+	;;
+esac
+
+#docker-compose run --rm openvpn easyrsa build-client-full $1 nopass
+#docker-compose run --rm openvpn ovpn_getclient $1 > $1.ovpn
+
