c7b232522ce7e7bb58e6873963f5a3a80b1fb00c
Josh 'Jekotia' Ameli <contact@jekotia.net>
2018-08-16 15:57:01 +0000

    Update env vars referenced for 'new' (I forgot to do this when I should have) naming scheme

--- ssl/run.sh
+++ ssl/run.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
-sslRoot=${SRV_ROOT}/ssl
-sslData=${SRV_ROOT}/data/ssl
+sslRoot=${_ROOT}/ssl
+sslData=${_ROOT}/data/ssl
 
 chmod +x ${sslRoot}/dns-01-manual/hook.sh
 $sslRoot/dehydrated/dehydrated --config $sslRoot/config $@
