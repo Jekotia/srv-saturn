a1a6d81631cad767a752931a2cdd4f9d24f92da8
Josh 'Jekotia' Ameli <contact@jekotia.net>
2018-11-13 14:41:54 -0500

    Update for cloudflare DNS for dns-01 challenge, using lexicon

--- ssl/run.sh
+++ ssl/run.sh
@@ -2,6 +2,19 @@
 sslRoot=${_ROOT}/ssl
 sslData=${_ROOT}/data/ssl
 
-chmod +x ${sslRoot}/dns-01-manual/hook.sh
-$sslRoot/dehydrated/dehydrated --config $sslRoot/config $@
-chmod -x ${sslRoot}/dns-01-manual/hook.sh
+cd $sslRoot/dehydrated
+./dehydrated --config $sslRoot/config $@
+
+if [ -f "/tmp/cert-update-success" ]; then
+	rm -rf /tmp/cert-update-success
+	echo "Restarting docker proxy"
+	docker restart proxy
+	if [ $? -eq 0 ] ; then
+		echo "Successfully restarted docker proxy"
+	else
+		echo "Failed to restart docker proxy"
+	fi
+fi
+
+exit $?
+
